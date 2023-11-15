ecada5cc97d2fd19c5017522bcb664b81416c8f9
samiulalimseam <samiulalimseam@gmail.com>
2023-11-15 17:59:52 +0600

    created project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
 
+import { Route, Routes } from 'react-router-dom'
+import './global.css'
+import SigninForm from './_auth/forms/SigninForm'
+import { Home } from './_root/pages'
+import { SignupForm } from './_auth/forms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <main className='flex h-screen'>
+            <Routes>
+                {/* public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-up' element={<SignupForm />} />
+                </Route>
+                {/* private routes */}
+                <Route element={<RootLayout />} >
+                <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
+    )
 }
 
 export default App
