db6e7ef1c2481ce8920191c7601926fa3d281906
samiulalimseam <samiulalimseam@gmail.com>
2023-11-16 15:19:10 +0600

    added assets in public

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './global.css'
 import SigninForm from './_auth/forms/SigninForm'
 import { Home } from './_root/pages'
-import { SignupForm } from './_auth/forms/SignupForm'
+import  SignupForm  from './_auth/forms/SignupForm'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 const App = () => {
--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Navigate, Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
@@ -7,9 +7,10 @@ const AuthLayout = () => {
     <> {isAuthenticated ? (<Navigate to={'/'} />) : 
     (
     <>
-    <section>
+    <section className='flex flex-1 justify-center items-center flex-col py-10'>
       <Outlet />
     </section>
+    <img src="/assets/images/side-img.svg" alt="side-img" className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat' />
     </>
     )} </>
   )
